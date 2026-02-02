d499c3068b18fb02f3472917be5494ba929bc469
JackScott7 <cloner.bl12@gmail.com>
2026-02-02 05:32:39 +0330

    Task Class and ActionStatus defined

--- utils.py
+++ utils.py
@@ -1,6 +1,15 @@
 from enum import Enum
 
 
+class Task:
+    def __init__(self, content, title, shorthand_title, task_id, tags):
+        self.content = content
+        self.title = title
+        self.shorthand_title = shorthand_title
+        self.task_id = task_id
+        self.tags = tags
+
+
 class TaskStatus(Enum):
     COMPLETED = "COMPLETED"
     ACTIVE = "ACTIVE"
@@ -10,3 +19,9 @@ class TaskStatus(Enum):
 class TaskReadType(Enum):
     PLAIN = "PLAIN"
     JSON = "JSON"
+
+
+class ActionStatus(Enum):
+    INTEGRITY_ERROR = "DUPLICATE_ERROR"
+    SUCCESS = "SUCCESS"
+    FAILURE = "FAILURE"
